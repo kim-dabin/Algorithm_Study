fdbb459fbfc69d6ba69f2c27ff39bbd9b1b8274e
kim-dabin <pippisgoing@gmail.com>
2019-09-20 18:24:59 +0900

    8104, 8338 문제 업로드

---
+++ src/KimDaBin/FirstWeek/Num8104.java
@@ -0,0 +1,43 @@
+package firstWeek;
+
+import java.util.Scanner;
+
+public class Num8104 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		int n = sc.nextInt(); // 조별당 인원 수 
+		int k = sc.nextInt(); // 조의 개수 
+		int result = 0;
+		int score = 0;
+		System.out.print("#"+test_case+" ");
+		if((n%2)==0) { // 짝수 
+			for(int i =1; i<=n; i+=2) {
+				result+=(2*i)*k;	
+			}//for end
+			score = result+(n/2);
+			for(int i=1; i<=k; i++) {
+				System.out.print(score+" ");
+			}
+		}else {
+			result = (n - 1)*((n - 1)*k + 1) / 2;
+			result+= (n - 1)*k + 1;
+//			result = ((2*k)*(n-1))+((n-1)/2)+1;
+			for(int i=0; i<k;i++) {
+				System.out.print(result+i+" ");
+			}
+		}
+			System.out.println();
+		}//for end test
+		
+	}
+
+}
--- src/KimDaBin/FirstWeek/Num8338.java
+++ src/KimDaBin/FirstWeek/Num8338.java
@@ -1,10 +1,37 @@
 package firstWeek;
 
+import java.util.Scanner;
+
 public class Num8338 {
-
+	/* 방탈출 카페 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		//test case
+		int T = scan.nextInt();
+		int testcase = 0;
+		while(T>testcase) {
+			int n = scan.nextInt();//정수 값 
+			int [] numArr = new int[n]; // N개의 정수 
+			
+			int result=0; //계산 결과 
+			
+			for(int i=0; i<n; i++) {
+				numArr[i] = scan.nextInt();
+			}
+			
+			for(int i =0; i<numArr.length;i++) {
+				
+				if(numArr[i]>1&&result>1) {
+					result*=numArr[i];
+				}else {
+					result+=numArr[i];
+				}
+			}//for end
+			testcase++;
+			System.out.println("#"+testcase+" "+result);
+			
+		}
 
-	}
+ 	}
 
 }
